07eac5325765a0ea95b54c44be13d6813b798cd5
ecolight15 <github@minecraftuser.jp>
2022-08-06 15:42:26 +0900

    チャット本文で各種装飾コードを&+英数字で使用できるよう修正

--- src/main/java/jp/minecraftuser/ecochatmqtt/timer/task/AsyncChatTaskChatReceive.java
+++ src/main/java/jp/minecraftuser/ecochatmqtt/timer/task/AsyncChatTaskChatReceive.java
@@ -214,10 +214,10 @@ public class AsyncChatTaskChatReceive extends AsyncChatTaskBase {
                 if (uschconf.italic) sb.append(ChatColor.ITALIC);
                 if (uschconf.line) sb.append(ChatColor.UNDERLINE);
                 if (uschconf.strike) sb.append(ChatColor.STRIKETHROUGH);
-                sb.append(json.msg);
+                sb.append(replaceColor(json.msg));
                 sb.append(ChatColor.RESET);
             } else {
-                sb.append(json.msg);
+                sb.append(replaceColor(json.msg));
             }
             
             // 送信リストに追加
@@ -257,4 +257,32 @@ public class AsyncChatTaskChatReceive extends AsyncChatTaskBase {
         log.info("return main therad = " + (System.nanoTime() - startTime) / 1000000);
     }
 
+    private String replaceColor(String msg) {
+        msg = msg.replaceAll("&0", ChatColor.BLACK.toString());
+        msg = msg.replaceAll("&1", ChatColor.DARK_BLUE.toString());
+        msg = msg.replaceAll("&2", ChatColor.DARK_GREEN.toString());
+        msg = msg.replaceAll("&3", ChatColor.DARK_AQUA.toString());
+        msg = msg.replaceAll("&4", ChatColor.DARK_RED.toString());
+        msg = msg.replaceAll("&5", ChatColor.DARK_PURPLE.toString());
+        msg = msg.replaceAll("&6", ChatColor.GOLD.toString());
+        msg = msg.replaceAll("&7", ChatColor.GRAY.toString());
+        msg = msg.replaceAll("&8", ChatColor.DARK_GRAY.toString());
+        msg = msg.replaceAll("&9", ChatColor.BLUE.toString());
+        msg = msg.replaceAll("&a", ChatColor.GREEN.toString());
+        msg = msg.replaceAll("&b", ChatColor.AQUA.toString());
+        msg = msg.replaceAll("&c", ChatColor.RED.toString());
+        msg = msg.replaceAll("&d", ChatColor.LIGHT_PURPLE.toString());
+        msg = msg.replaceAll("&e", ChatColor.YELLOW.toString());
+        msg = msg.replaceAll("&f", ChatColor.WHITE.toString());
+
+        msg = msg.replaceAll("&k", ChatColor.MAGIC.toString());
+        msg = msg.replaceAll("&l", ChatColor.BOLD.toString());
+        msg = msg.replaceAll("&m", ChatColor.STRIKETHROUGH.toString());
+        msg = msg.replaceAll("&n", ChatColor.UNDERLINE.toString());
+        msg = msg.replaceAll("&o", ChatColor.ITALIC.toString());
+        msg = msg.replaceAll("&r", ChatColor.RESET.toString());
+
+        return msg;
+    }
 }
+
